2eaaf05c367112c43684122997ad95cb2ac4d16d
Alfie <50023887+bemyrequiem@users.noreply.github.com>
2023-02-08 14:20:43 +0000

    Just basic start

---
+++ dungeon_game/main.py
@@ -0,0 +1,16 @@
+# ================================================
+# TEAM SOFTWARE ENGINEERING ASSIGNMENT 3 - GROUP 5
+# ================================================
+# Alfie Atkinson, Alex Clarke, Jamie Grant,
+# Kamil Marszalek, Craig Potter, Lukas Sobolevas
+# ================================================
+# VERSION 0.1 - Week Commencing 06/02/2023
+# ================================================
+
+import menus
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
